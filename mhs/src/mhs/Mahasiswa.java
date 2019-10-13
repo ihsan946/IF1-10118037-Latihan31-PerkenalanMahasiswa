47a946602241c51c76b381ad0b5679e5a4ac4ff1
Muhammad Ihsan <55564001+ihsan946@users.noreply.github.com>
2019-10-13 14:39:05 +0700

    Add files via upload

---
+++ mhs/src/mhs/Mahasiswa.java
@@ -0,0 +1,16 @@
+package mhs;
+
+public class Mahasiswa {
+
+	public String nim;
+	public String nama;
+
+	public void perkenalanDiri() {
+	      
+                          System.out.println("Hallo Everyone");
+                          System.out.println("My NIM is " + nim);
+                          System.out.println("My name is " + nama);
+                          System.out.println();
+	}
+
+}
---
+++ mhs/src/mhs/Mhs.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mhs;
+
+
+/**
+ *
+ * @author Muhammad Ihsan
+ * Nama : Muhammad Ihsan
+ * Nim   : 10118037
+ * Kelas : IF-1 2018
+ * Deskripsi program : Program ini menampilkan nim dan nama mahasiswa
+ */
+public class Mhs {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+            Mahasiswa mhs = new Mahasiswa();
+           
+            
+            //output
+            mhs. nim = "10118037";
+            mhs.nama ="Muhammad Ihsan";
+            mhs.perkenalanDiri();
+            
+            mhs.nim = "10118023";
+            mhs.nama = "Zaenal Anzzary";
+            mhs.perkenalanDiri();
+            
+        
+    }
+    
+}
